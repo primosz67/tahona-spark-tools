25d7e0329e71605244dd4b40c29e0dd6bd593d1a
The King <crownclown67@gmail.com>
2018-04-23 07:48:47 +0200

    CSRF some urls can be excluded now

--- src/Spark/Security/Csrf/CsrfSecurityFilter.php
+++ src/Spark/Security/Csrf/CsrfSecurityFilter.php
@@ -5,15 +5,19 @@ namespace Spark\Security\Csrf;
 use Spark\Core\Annotation\Inject;
 use Spark\Core\Filter\FilterChain;
 use Spark\Core\Filter\HttpFilter;
+use Spark\Core\Routing\RequestData;
 use Spark\Http\Request;
 use Spark\Http\ResponseHelper;
 use Spark\Http\Utils\RequestUtils;
 use Spark\Security\Csrf\Exception\BadCsrfException;
+use Spark\Utils\Collections;
 use Spark\Utils\Objects;
+use Spark\Utils\StringUtils;
 
 class CsrfSecurityFilter implements HttpFilter {
 
     private $formKey;
+    private $excludedPaths;
 
     /**
      * CsrfSecurityFilter constructor.
@@ -21,6 +25,7 @@ class CsrfSecurityFilter implements HttpFilter {
      */
     public function __construct(string $formKey) {
         $this->formKey = $formKey;
+        $this->excludedPaths = [];
     }
 
     /**
@@ -29,8 +34,7 @@ class CsrfSecurityFilter implements HttpFilter {
      * @throws BadCsrfException
      */
     public function doFilter(Request $request, FilterChain $filterChain) {
-
-        if ($request->isPost()) {
+        if ($request->isPost() && !$this->isExcluded($request)) {
             $csrf = $request->getParam($this->formKey);
             $session = $request->getSession();
 
@@ -42,5 +46,22 @@ class CsrfSecurityFilter implements HttpFilter {
                 throw new BadCsrfException('Bad Csrf for POST request');
             }
         }
+    }
+
+    public function exclude(array $paths): void {
+        Collections::addAll($this->excludedPaths, $paths);
+    }
+
+    private function isExcluded(Request $request): bool {
+        if ($request instanceof RequestData) {
+            /** @var RequestData $request */
+            $requestPath = $request->getRouteDefinition()->getPath();
+
+            return Collections::anyMatch($this->excludedPaths, function ($p) use ($requestPath) {
+                return StringUtils::startsWith($requestPath, $p);
+            });
+        }
+
+        return false;
     }
 }
