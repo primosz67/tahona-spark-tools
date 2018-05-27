a76867ba430600310159497d00bb37baebeb961f
John Tribe <crownclown67@gmail.com>
2018-05-27 16:45:57 +0200

    added decode HtmlUtils

--- src/Spark/Tools/Html/HtmlUtils.php
+++ src/Spark/Tools/Html/HtmlUtils.php
@@ -25,4 +25,15 @@ class HtmlUtils {
     public static function builder($rootTag="span") {
         return new HtmlElement($rootTag);
     }
+
+    /**
+     * Change this : A &#039;quote&#039; is &lt;b&gt;bold&lt;/b&gt
+     * Into this "A 'quote' is <b>bold</b>"
+     *
+     * @param string $value
+     * @return string
+     */
+    public static function decode(?string $value) :string {
+        return htmlspecialchars_decode($value);
+    }
 }
