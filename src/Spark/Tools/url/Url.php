93c6c794e230b9777ac3094d7e5cccab02109f37
John Tribe <crownclown67@gmail.com>
2018-06-29 23:10:07 +0200

    url extension

--- src/Spark/Tools/url/Url.php
+++ src/Spark/Tools/url/Url.php
@@ -24,9 +24,9 @@ class Url {
     public function __construct($url) {
         $arr = parse_url($url);
 
-        $this->scheme = $arr["scheme"];
-        $this->host = $arr["host"];
-        $this->path = Collections::getValueOrDefault($arr, "path", "");
+        $this->scheme = $arr['scheme'];
+        $this->host = $arr['host'];
+        $this->path = Collections::getValueOrDefault($arr, 'path', '');
         $this->queryParams = $this->parseQuery($this->getQueryString($arr));
 
     }
@@ -34,7 +34,7 @@ class Url {
     public function build() {
         $aar = array(
             $this->scheme,
-            "://",
+            '://',
             $this->host,
             $this->getPort(),
             $this->path,
@@ -42,7 +42,7 @@ class Url {
         );
 
         $res = Collections::filter($aar, Predicates::notNull());
-        return StringUtils::join("", $res);
+        return StringUtils::join('', $res);
     }
 
     public static function parse($url) {
@@ -53,16 +53,16 @@ class Url {
         if (Collections::isNotEmpty($this->queryParams)) {
             $values = array();
             foreach ($this->queryParams as $k => $q) {
-                $values[] = $k . "=" . $q;
+                $values[] = $k . '=' . $q;
             }
-            return "?" . StringUtils::join("&", $values);
+            return '?' . StringUtils::join('&', $values);
         }
         return null;
     }
 
     private function getPort() {
         if (StringUtils::isNotBlank($this->port)) {
-            return ":" . $this->port;
+            return ':' . $this->port;
         }
         return null;
     }
@@ -70,12 +70,12 @@ class Url {
     private function parseQuery(string $query=null) {
 
         if (StringUtils::isNotBlank($query)) {
-            $queries = StringUtils::split($query, "&");
+            $queries = StringUtils::split($query, '&');
             $res = array();
 
             foreach ($queries as $q) {
-                $keyValue = StringUtils::split($q, "=");
-                $res[$keyValue[0]] = Collections::getValueOrDefault($keyValue, 1, "");
+                $keyValue = StringUtils::split($q, '=');
+                $res[$keyValue[0]] = Collections::getValueOrDefault($keyValue, 1, '');
             }
             return $res;
         }
@@ -103,8 +103,12 @@ class Url {
 
     private function getQueryString($arr){
         return Optional::ofNullable($arr)
-            ->map(Functions::getArrayValue("query"))
-            ->orElse("");
+            ->map(Functions::getArrayValue('query'))
+            ->orElse('');
+    }
+
+    public function getPath() {
+        return $this->path;
     }
 
 }
